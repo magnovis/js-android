48f86dfc2e7c5543b609b181e20edabde9f33090
M <beny.tan@gmail.com>
2017-12-12 13:42:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -364,7 +364,7 @@
   // TODO add service worker code here
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./service-worker.js')
+             .register('https://cdn.rawgit.com/magnovis/js-android/bf0e081e/service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
